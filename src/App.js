607387be752a754c4e4ca1a23e09f700d13651e2
Romain Gonord <romain.gonord.opensource@neteyes.org>
2020-03-22 20:50:37 +0100

    tabletop first try ... really simple to use

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Tabletop from 'tabletop';
 
 function App() {
-	return <div>TODO </div>;
+	useEffect(() => {
+		async function fetchData() {
+			const data = await Tabletop.init({
+				key: process.env.REACT_APP_SPREADSHEET_URL,
+				simpleSheet: true,
+			});
+			console.log(data);
+		}
+		fetchData();
+	}, []);
+	return <div>TODO</div>;
 }
 
 export default App;
